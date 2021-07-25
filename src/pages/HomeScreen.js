3e17647d053df3e49939061c35257f2935163797
Ruan Castro <ruancastro15@gmail.com>
2021-07-25 12:03:44 -0300

    sem readme

---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+export default function HomeScreen() {
+    return (
+        <div className="container">
+            <h1 align="center">Seja bem vindo(a) a home page de staff da Donna Doçuras </h1>
+            <br/><br/> <p align="center">No slidebar à sua esquerda você pode ter acesso a nossa lista de clientes e produtos, sendo possível também cadastrar novos.<br/> <img src="logo.png" width="300" height="300"></img>  </p><br/>
+            
+
+        
+        </div>
+        
+        
+       
+    )
+}
